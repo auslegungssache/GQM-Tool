a07a71b98aeb0fc82cc7de47383c9b97de974a6d
auslegungssache <me@werl.dev>
2024-11-11 21:29:18 +0100

    chore: remove old code

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import CreateProject from "@/app/app/_create/create-project";
 
 export default async function Notes() {
   return (
     <>
       <div>test</div>
-      <CreateProject />
     </>
   );
 }
